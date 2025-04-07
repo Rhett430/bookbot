4c889e6cb2809ed72ac1edf0487e478e56f85fd1
Rhett430 <rhett430@gmail.com>
2025-04-07 15:51:17 -0400

    BookBot done!

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
-from stats import get_book_text
-from stats import get_char_count
-from stats import sort_char_count
+from stats import(
+    get_word_count
+    ,get_char_count,
+    sort_char_count)
+import sys
 def main():
-    word_count = 0
-    char_count = get_char_count("./books/frankenstein.txt")
-    frank = get_book_text("./books/frankenstein.txt")
-    words=frank.split()
-    for word in words:
-        word_count += 1
-    print(f"{word_count} words found in the document")
-    print(char_list)
+        if len(sys.argv) != 2:
+            print("Usage: python3 main.py <path_to_book>")
+            sys.exit(1)
+        else:
+            book_path = sys.argv[1]
+            text = get_book_text(book_path)
+            word_count = get_word_count(text)
+            char_count = get_char_count(text)
+            sorted_char_count = sort_char_count(char_count)
+            print_report(book_path, word_count, sorted_char_count)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+ 
+def print_report(book_path,word_count,sorted_char_count):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for char in sorted_char_count:
+        if char["char"].isalpha():
+            print(f"{char["char"]}: {char["count"]}")
+    
+    print("============= END ===============")
+
 main()
--- stats.py
+++ stats.py
@@ -1,17 +1,14 @@
-def get_book_text(path_to_file):
-    with open(path_to_file) as f:
-        file_contents = f.read()
-    return file_contents
+def get_word_count(text):
+    words = text.split()
+    return len(words)
 
-def get_char_count(path_to_file):
+def get_char_count(text):
     char_count = {}
-    with open(path_to_file) as f:
-        file_contents = f.read()
-        for char in file_contents.lower():
-            if char in char_count:
-                char_count[char] += 1
-            else:
-                char_count[char] = 1
+    for char in text.lower():
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
     return(char_count)
 
 def sort_on(dict):
